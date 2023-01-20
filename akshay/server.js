56e645059b4525cd046f2b7afdb099c0f858b9ed
dac <dac@dypm.org>
2023-01-20 17:10:17 +0530

    hi

---
+++ akshay/server.js
@@ -0,0 +1,18 @@
+var express=require("express");
+ var app=express();
+
+ var path=require("path");
+ app.get('/',function(req,resp){
+    resp.sendFile(path.join(__dirname,'public/app.html'))
+   
+});
+ app.get('/home',function(req,resp){
+    resp.sendFile(path.join(__dirname,'public/app2.html'))
+ });
+ app.get('/about',function(req,resp){
+resp.send("<h1>hello</h1>");
+    
+ });
+
+console.log("you are on port 8081")
+ app.listen(5500)
